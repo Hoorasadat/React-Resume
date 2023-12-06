e5755f727cabc0aa5dfdb1f3ee32d5891bb7855a
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-12-06 13:29:56 -0700

    fix the repo file to fetch all repos

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,25 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
-const Repositories = ( {styles} ) => {
+const Repositories = ({ styles }) => {
   const [repositories, setRepositories] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    // const accessToken = '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';
+  const fetchRepositories = async () => {
+    const apiUrl = 'https://api.github.com/users/hoorasadat/repos';
+    let allRepos = [];
 
-    fetch('https://api.github.com/users/hoorasadat/repos', {
-      method: 'GET'
-    })
-      .then(response => response.json())
-      .then(data => {
-        setRepositories(data);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setLoading(false);
-      });
+    try {
+      let page = 1;
+      let hasNextPage = true;
+
+      while (hasNextPage) {
+        const response = await fetch(`${apiUrl}?page=${page}&per_page=30`);
+        const data = await response.json();
+
+        if (Array.isArray(data) && data.length > 0) {
+          allRepos = [...allRepos, ...data];
+          page++;
+        } else {
+          hasNextPage = false;
+        }
+      }
+
+      // Sort repositories chronologically, newest first
+      allRepos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+      setRepositories(allRepos);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRepositories();
   }, []);
 
   if (loading) {
@@ -36,22 +54,26 @@ const Repositories = ( {styles} ) => {
         <div className="card-body">
           <h2 className="card-title mb-5">My GitHub Repositories</h2>
           <ul className="list-unstyled">
-						{repositories.length > 0 ? (repositories.map(repo => (
-						<li key={repo.id}>
-							<a
-								href={repo.html_url} target="_blank" rel="noopener noreferrer"
-								className="fs-6 text-decoration-none"
-							>
-								{repo.name}
-							</a>
-						</li>
-						))) :
-            (<li className="fs-5 text-danger">It seems that the access token for repositories has expired!</li>)
-            }
-					</ul>
-				</div>
-			</div>
-		</div>
+            {repositories.length > 0 ? (
+              repositories.map(repo => (
+                <li key={repo.id}>
+                  <a
+                    href={repo.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-decoration-none"
+                  >
+                    {repo.name}
+                  </a>
+                </li>
+              ))
+            ) : (
+              <li className="fs-5 text-danger">Error fetching repositories!</li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
